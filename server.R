b3b5581bc37c9cbc6b8ef464b8b4c2f82d79d264
Henrique-Shiguemoto <shiguemoto88@gmail.com>
2024-04-18 23:29:13 -0300

    Initial commit, template setup

---
+++ server.R
@@ -0,0 +1,12 @@
+library(shiny)
+library(shinyjs)
+library(shinycustomloader)
+library(rio)
+library(tidyverse)
+library(ggplot2)
+library(shinydashboard)
+
+shinyServer(function(input, output){
+  # uncomment this line if you wanna debug with shinyjs
+  # runcodeServer()
+})
