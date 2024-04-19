b7ff12e6dc011cf6a42594c02e62a4e3f31d4d75
Henrique-Shiguemoto <shiguemoto88@gmail.com>
2024-04-18 23:47:18 -0300

    We'll be using highcharter for ther visualizations

--- ui.R
+++ ui.R
@@ -2,19 +2,14 @@ library(shiny)
 library(shinyjs)
 library(shinycustomloader)
 library(shinydashboard)
-library(leaflet)
-
-# I usually like to use non-white colors for boxes' background color
-background_box_color = "light-blue"
+library(highcharter)
 
 shinyUI(
   dashboardPage(
     dashboardHeader(title = "Dashboard Template"),
     dashboardSidebar(
       sidebarMenu(
-        menuItem("Tab1", tabName = "tab1"),
-        menuItem("Tab2", tabName = "tab2"),
-        menuItem("Tab3", tabName = "tab3")
+        menuItem("Tab1", tabName = "tab1")
         # uncomment this line if you wanna debug with shinyjs
         # useShinyjs(),
         # runcodeUI(type = "textarea")
@@ -23,9 +18,7 @@ shinyUI(
     dashboardBody(
       tabItems(
         # Add elements such as boxes, fluidRows and columns after the tabName parameters
-        tabItem(tabName = "tab1"),
-        tabItem(tabName = "tab2"),
-        tabItem(tabName = "tab3")
+        tabItem(tabName = "tab1")
       )
     )
   )
